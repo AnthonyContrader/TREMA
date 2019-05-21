b02a598bec664e12bc77fa1eb4abe250f3d744fa
adrypap <papucmarinadrian@gmail.com>
2019-05-21 12:08:44 +0200

    commit prova

--- src/main/java/it/contrader/main/Application.java
+++ src/main/java/it/contrader/main/Application.java
@@ -1,9 +1,34 @@
 package it.contrader.main;
+import java.util.*;
 
 public class Application {
 
     public static void main(String[] args) {
         MainDispatcher.getInstance().callAction("Login", "doControl", null);
+        
+        persona p=new persona();
+        materiale m=new materiale();
+        
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+
+        int a=0;
+        String test;
+        
+        System.out.println("Benvenuto in TREMA!!");
+        do {
+        	System.out.println("Possibile menù...");
+        	test=input.nextLine();
+        	a=Integer.parseInt(test);
+        	switch(a) {
+        						case 1:
+        							break;
+        						case 2:
+        							break;
+        	}
+        	
+        }while(a!=70);
+       
     }
 
 }
---
+++ src/main/java/it/contrader/main/materiale.java
@@ -0,0 +1,5 @@
+package it.contrader.main;
+
+public class materiale {
+
+}
