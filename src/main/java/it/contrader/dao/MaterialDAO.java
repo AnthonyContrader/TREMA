ab129baac9ccd59bf6a9bb82568065d752ba9c1f
Adma76 <amatasanchez@hotmail.com>
2019-05-23 15:39:46 +0200

    commit prova

---
+++ src/main/java/it/contrader/dao/MaterialDAO.java
@@ -0,0 +1,123 @@
+package it.contrader.dao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.controller.GestoreEccezioni;
+import it.contrader.main.ConnectionSingleton;
+import it.contrader.model.Ambiente;
+import it.contrader.model.Material;
+
+public class MaterialDAO {
+	
+	private final String QUERY_ALL = "SELECT * FROM material WHERE idHR=?";
+	private final String QUERY_INSERT = "INSERT INTO material (tipo, building) VALUES (?,?)";
+	private final String QUERY_READ = "SELECT * FROM material WHERE idHR=? AND  idmaterial=?";
+	private final String QUERY_UPDATE = "UPDATE material SET tipo=?, idHR=? WHERE idmaterial=?";
+	private final String QUERY_DELETE = "DELETE FROM material WHERE idmaterial=?";
+	
+	public MaterialDAO() {}
+	
+	public List<Material> showAllMaterial(int idHR) {
+		List<Material> materials = new ArrayList<>();
+		Connection connection = ConnectionSingleton.getInstance();
+		
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_ALL);
+			preparedStatement.setInt(1, idHR);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			Material material;
+			
+			while (resultSet.next()) {
+				int idmaterial = resultSet.getInt("idmaterial");
+				String descrizione = resultSet.getString("tipo");
+				int idhr = resultSet.getInt("idHR");
+				
+				material = new Material(descrizione, idhr);
+				material.setId(idmaterial);
+				materials.add(material);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return materials;
+	}
+
+	public boolean insertMaterial(Material material) {
+		Connection connection = ConnectionSingleton.getInstance();
+		
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_INSERT);
+			preparedStatement.setString(1, material.getDescrizione());
+			preparedStatement.setInt(2, material.getBuildingid());
+			preparedStatement.execute();
+
+			return true;
+
+		} catch (SQLException e) {
+			GestoreEccezioni.getInstance().gestisciEccezione(e);
+			return false;
+		}
+
+	}
+
+	public Material readMaterial(int idhr, int idmaterial) {
+		Connection connection = ConnectionSingleton.getInstance();
+
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_READ);
+			preparedStatement.setInt(1,idhr);
+			preparedStatement.setInt(2,idmaterial);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			resultSet.next();
+
+			String descrizione= resultSet.getString("tipo");
+			idhr = resultSet.getInt("idHR");
+
+			Material material = new Material (descrizione,idhr);
+			material.setId(resultSet.getInt("idmaterial"));
+
+			return material;
+		} 
+		catch (SQLException e) {
+			GestoreEccezioni.getInstance().gestisciEccezione(e);
+
+			return null;
+		}
+	}
+
+	public boolean updateMaterial(Material materialToUpdate) {
+		Connection connection = ConnectionSingleton.getInstance();
+		try {
+			PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(QUERY_UPDATE);
+			preparedStatement.setString(1, materialToUpdate.getDescrizione());
+			preparedStatement.setInt(2, materialToUpdate.getBuildingid());
+			preparedStatement.setInt(3, materialToUpdate.getIdMaterial());
+			preparedStatement.executeUpdate();
+		} 
+		catch (SQLException e) {
+			System.out.println("Error...\n");
+			return false;
+		}
+
+		return true;
+	}		
+	
+	public boolean deleteMaterial(int idmaterial) {
+		Connection connection = ConnectionSingleton.getInstance();
+		
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(QUERY_DELETE);
+			preparedStatement.setInt(1, idmaterial);
+			int n = preparedStatement.executeUpdate();
+			
+			if (n != 0)
+				return true;
+		} catch (SQLException e) {
+		}
+		return false;
+	}
+
+}
