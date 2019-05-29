3d8b4f4912822e92f77af634e3f35dfc18662c03
adrypap <papucmarinadrian@gmail.com>
2019-05-29 09:12:16 +0200

    adrian

---
+++ src/main/java/it/contrader/servlets/HumanResourceServlet.java
@@ -0,0 +1,12 @@
+package it.contrader.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HumanResourceServlet extends HttpServlet{
+
+}
