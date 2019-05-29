ab72bb739a28c165e0f824eb1e79f73d5b16e8f7
Sibillano95 <gianluca.95@hotmail.com>
2019-05-29 16:46:03 +0200

    gianluca

--- src/main/java/it/contrader/servlets/LoginServlet.java
+++ src/main/java/it/contrader/servlets/LoginServlet.java
@@ -2,44 +2,44 @@ package it.contrader.servlets;
 
 import java.io.IOException;
 
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import it.contrader.dto.UsersDTO;
+import it.contrader.service.ServiceDTO;
 import it.contrader.service.UsersServiceDTO;
 
 public class LoginServlet extends HttpServlet {
-	private final UsersServiceDTO usersServiceDTO = new UsersServiceDTO();
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		final HttpSession session = request.getSession();
 		session.setAttribute("utente", null);
 
+		ServiceDTO<UsersDTO> service = new UsersServiceDTO();
+
 		if (request != null) {
-			final String username = request.getParameter("username").toString();
-			final String password = request.getParameter("password").toString();
-			// recuperiamo l'utente
-			final UsersDTO usersDTO = usersServiceDTO.getUserByUsernameAndPasword(username, password);
+			String username = request.getParameter("username").toString();
+			String password = request.getParameter("password").toString();
+			UsersDTO dto = service.read(username, password);
+			if (dto != null)
+				session.setAttribute("users", dto);
+			else
+				getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
 
-			if (usersDTO != null)
-				session.setAttribute("utente", usersDTO);
-
-			// verifichiamo che tipo di ruolo ha all'interno dell'applicazione
-			// e lo reindirizziamo nella jsp opportuna
-			switch (usersDTO.getUsertype()) {
-			case "admin":
-				getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
+			switch (dto.getUsertype().toUpperCase()) {
+			case "ADMIN":
+				getServletContext().getRequestDispatcher("/homeadmin.jsp").forward(request, response);
 				break;
-			case "project_manager":
-				getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);//da inserire la home page di PM
+			case "HR_MANAGER":
+				getServletContext().getRequestDispatcher("/homeHR.jsp").forward(request, response);
 				break;
-			case "hr_manager":
-				getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);//da inserire la home page di HR manager
+			case "PJ_MANAGER":
+				getServletContext().getRequestDispatcher("/homePJ.jsp").forward(request, response);
 				break;
 			default:
 				getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
@@ -47,5 +47,4 @@ public class LoginServlet extends HttpServlet {
 			}
 		}
 	}
-
 }
--- src/main/java/it/contrader/servlets/LogoutServlet.java
+++ src/main/java/it/contrader/servlets/LogoutServlet.java
@@ -8,7 +8,11 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class LogoutServlet extends HttpServlet {
-    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         session.invalidate();
         response.sendRedirect("index.jsp");
