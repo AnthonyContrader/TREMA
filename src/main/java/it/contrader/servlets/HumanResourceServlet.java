e6f1e69cdf9988dbcda69f6107d1fd8140256785
adrypap <papucmarinadrian@gmail.com>
2019-05-29 16:03:54 +0200

    adrian

--- src/main/java/it/contrader/servlets/HumanResourceServlet.java
+++ src/main/java/it/contrader/servlets/HumanResourceServlet.java
@@ -1,7 +1,6 @@
 package it.contrader.servlets;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -15,7 +14,6 @@ public class HumanResourceServlet extends HttpServlet {
 		private static final long serialVersionUID = 1L;
 		private int idhr;
 		private HttpSession session;
-		private int iduser;
 		public HumanResourceServlet() {}
 		
 		public void updateList(HttpServletRequest request) {
@@ -29,78 +27,75 @@ public class HumanResourceServlet extends HttpServlet {
 		public void service (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			ServiceDTO<HumanResourceDTO> service = new HumanResourceServiceDTO();
 		    
-		    if(request.getParameter("idhr")!=null) {
+		    if(request.getParameter("idHR")!=null) {
 		    	idhr = Integer.parseInt(request.getParameter("idHR").toString());
 		    }
-
-			if(request.getParameter("id")!=null) {
-				id = Integer.parseInt(request.getParameter("id"));
-		    }
 			
-			ItemDTO dto;
+		    HumanResourceDTO hrdto;
 			boolean ans;
-			String description, itemType;
+			int iduser;
+			String name, surname;
 			
 			
 		    String mode = request.getParameter("mode");
 			
 			session = request.getSession();
-			session.setAttribute("ambientId",ambientId);
+			session.setAttribute("idHR",idhr);
 			
 			switch (mode.toUpperCase()) {
 
-			case "ITEMLISTBY":
+			case "HRLISTBY":
 				updateList(request);
-				getServletContext().getRequestDispatcher("/item/itemmanager.jsp").forward(request, response);
+				getServletContext().getRequestDispatcher("/item/hrmanager.jsp").forward(request, response);
 				break;
 
-			case "ITEMLISTOP":
+			case "HRLISTTOP":
 				updateList(request);
-				getServletContext().getRequestDispatcher("/homeoperatore.jsp").forward(request, response);
+				getServletContext().getRequestDispatcher("/hrhome.jsp").forward(request, response);
 				break;
 
 			case "READ":
-				dto = service.read(id);
-				request.setAttribute("dto", dto);
-				getServletContext().getRequestDispatcher("/item/readitem.jsp").forward(request, response);
+				hrdto = service.read(idhr);
+				request.setAttribute("hrdto", hrdto); //possibile error
+				getServletContext().getRequestDispatcher("/item/hrread.jsp").forward(request, response);
 				break;
 
 			case "INSERT":
-				description= request.getParameter("description");
-				itemType= request.getParameter("itemType");
-				int ambientId = Integer.parseInt(request.getParameter("ambientId"));
-				dto = new ItemDTO (description, itemType, ambientId);
-				ans = service.insert(dto);
+				name= request.getParameter("name");
+				surname= request.getParameter("surname");
+				iduser = Integer.parseInt(request.getParameter("iduser"));
+				hrdto = new HumanResourceDTO (name, surname, iduser);
+				ans = service.insert(hrdto);
 				request.setAttribute("ans", ans);
 				updateList(request);
-				getServletContext().getRequestDispatcher("/item/itemmanager.jsp").forward(request, response);
+				getServletContext().getRequestDispatcher("/item/hrinsert.jsp").forward(request, response);
 				break;
 
 			case "PREUPDATE":
-				dto = service.read(id);;
-				request.setAttribute("dto", dto);
-				getServletContext().getRequestDispatcher("/item/updateitem.jsp").forward(request, response);
+				hrdto = service.read(idhr);
+				request.setAttribute("hrdto", hrdto);
+				getServletContext().getRequestDispatcher("/item/hrpreupdate.jsp").forward(request, response);
 				break;
 
 			case "UPDATE":
-				description = request.getParameter("description");
-				itemType= request.getParameter("itemType");
-				ambientId = Integer.parseInt(request.getParameter("ambientId"));
+				name = request.getParameter("name");
+				surname= request.getParameter("surname");
+				iduser = Integer.parseInt(request.getParameter("iduser"));
 				int idToUpdate = Integer.parseInt(request.getParameter("idToUpdate"));
-				dto = new ItemDTO(idToUpdate,description, itemType, ambientId);
-				ans = service.update(dto);
+				hrdto = new HumanResourceDTO(idToUpdate,name, surname, iduser);
+				ans = service.update(hrdto);
 				request.setAttribute("ans", ans);
 				System.out.println(ans);
 				updateList(request);
-				getServletContext().getRequestDispatcher("/item/itemmanager.jsp").forward(request, response);
+				getServletContext().getRequestDispatcher("/item/hrupdate.jsp").forward(request, response);
 				break;
 
 			case "DELETE":
-				dto = service.read(id);
-				ans = service.delete(dto);
+				hrdto = service.read(idhr);
+				ans = service.delete(hrdto);
 				request.setAttribute("ans", ans);
 				updateList(request);
-				getServletContext().getRequestDispatcher("/item/itemmanager.jsp").forward(request, response);
+				getServletContext().getRequestDispatcher("/item/hrdelete.jsp").forward(request, response);
 				break;
 			}
 		}
