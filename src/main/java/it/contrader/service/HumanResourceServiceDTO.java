5e5df1d93e7cfb8f41bd05a77d5408b41ad5325c
adrypap <papucmarinadrian@gmail.com>
2019-05-30 15:26:01 +0200

    adrian

--- src/main/java/it/contrader/service/HumanResourceServiceDTO.java
+++ src/main/java/it/contrader/service/HumanResourceServiceDTO.java
@@ -1,13 +1,64 @@
 package it.contrader.service;
+import java.util.ArrayList;
+import java.util.List;
+
 import it.contrader.converter.*;
 import it.contrader.dao.*;
 import it.contrader.dto.*;
 import it.contrader.model.*;
 
-public class HumanResourceServiceDTO extends AbstractServiceDTO<HumanResource,HumanResourceDTO>{
-	
+public class HumanResourceServiceDTO {
+
+
+/**
+ * Classe che si occupa di interfacciarsi con la persistenza e recuperare
+ * attraverso i metodi del Data Access Object le tuple desiderate, Le converte
+ * in un oggetto DTO e le restituisce al controller opportuno
+ */
+
+	private final HumanResourceDAO hrDAO;
+
 	public HumanResourceServiceDTO() {
-		dao = new HumanResourceDAO();
-		converter = new HumanResourceConverter();
+		this.hrDAO = new HumanResourceDAO();
 	}
+
+	/**
+	 * Come vediamo la lista recuperata � di tipo Esempio ma noi la convertiamo in EsempioDTO
+	 * Invito chi fa i converter a fare un metodo per convertire direttamente la lista senza farli uno ad uno perch� � sporco e poco efficiente
+	 */
+	public List<HumanResourceDTO> getAllClient() {
+
+		List<HumanResource> list = hrDAO.getAllHr();
+		List<HumanResourceDTO> listDTO = new ArrayList<>();
+
+		for (HumanResource hr : list) {
+			listDTO.add(HumanResourceConverter.toDTO(hr));
+		}
+
+		return listDTO;
+	}
+	
+
+	
+	public HumanResourceDTO readHr(HumanResourceDTO hr) {
+		return HumanResourceConverter.toDTO(this.hrDAO.readHr(HumanResourceConverter.toEntity(hr)));
+	}
+
+	public boolean updateHr(HumanResourceDTO clientDTO) {
+		return this.hrDAO.updateHr(HumanResourceConverter.toEntity(clientDTO));
+		
+}
+	
+	public boolean deleteHr(HumanResourceDTO hr) {
+		return this.hrDAO.deleteHr(HumanResourceConverter.toEntity(hr));
+		
+}
+	
+	public boolean insertHr(HumanResourceDTO hr) {
+		return this.hrDAO.insertHr(HumanResourceConverter.toEntity(hr));
+	
+}
+		
+	
+	
 }
--- src/main/java/it/contrader/servlets/HumanResourceServlet.java
+++ src/main/java/it/contrader/servlets/HumanResourceServlet.java
@@ -1,6 +1,7 @@
 package it.contrader.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -9,95 +10,100 @@ import javax.servlet.http.*;
 import it.contrader.dto.*;
 import it.contrader.service.*;
 
-public class HumanResourceServlet extends HttpServlet {
-		
-		private static final long serialVersionUID = 1L;
-		private int idhr;
-		private HttpSession session;
-		public HumanResourceServlet() {}
-		
-		public void updateList(HttpServletRequest request) {
-			session.getAttribute("idhr" );
-			ServiceDTO<HumanResourceDTO> service = new HumanResourceServiceDTO();
-			List<HumanResourceDTO> listDTO = service.getAll();
-			request.setAttribute("list", listDTO);
+public class ClientServlet extends HttpServlet {
+
+	private ClientServiceDTO clientServiceDTO = new ClientServiceDTO();
+	private List<ClientDTO> allClients = new ArrayList<ClientDTO>();
+	private List<ClientDTO> filteredClients = new ArrayList<ClientDTO>();
+
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		final String scelta = request.getParameter("richiesta");
+		final HttpSession session = request.getSession(true);
+		final UserDTO userLogged = (UserDTO) session.getAttribute("utente");
+
+		switch (scelta) {
+
+		case "ClientManager":
+			showAllClient(request, response);
+			break;
+
+		case "insertRedirect":
+			response.sendRedirect("client/insertClient.jsp");
+			break;
+
+		case "insert":
+			// final int userId = Integer.parseInt(request.getParameter("user_id"));
+			final String clientName = request.getParameter("client_name");
+			// UserDTO userInsert = new UserDTO("","","");
+
+			// userInsert.setId(userId);
+			final ClientDTO clientInsert = new ClientDTO(userLogged, clientName);
+			clientServiceDTO.insertClient(clientInsert);
+			showAllClient(request, response);
+			break;
+
+		case "updateRedirect":
+			int id = Integer.parseInt(request.getParameter("id"));
+			ClientDTO clientUpdate = new ClientDTO(new UserDTO("", "", ""), "");
+			clientUpdate.setId(id);
+
+			clientUpdate = this.clientServiceDTO.readClient(clientUpdate);
+			request.setAttribute("clientUpdate", clientUpdate);
+			getServletContext().getRequestDispatcher("/client/updateClient.jsp").forward(request, response);
+
+			break;
+
+		case "update":
+			final Integer clientIdUpdate = Integer.parseInt(request.getParameter("client_id"));
+			// final Integer userIdUpdate =
+			// Integer.parseInt(request.getParameter("user_id"));
+			final String clientNameUpdate = request.getParameter("client_name");
+
+			final ClientDTO clientDTO = new ClientDTO(userLogged, clientNameUpdate);
+			clientDTO.setId(clientIdUpdate);
+			clientServiceDTO.updateClient(clientDTO);
+			showAllClient(request, response);
+			break;
+
+		case "delete":
+			final Integer clientIdDelete = Integer.parseInt(request.getParameter("id"));
+
+			final ClientDTO clientdelete = new ClientDTO(userLogged, "");
+			clientdelete.setId(clientIdDelete);
+			clientServiceDTO.deleteClient(clientdelete);
+			showAllClient(request, response);
+			break;
+
+		case "indietro":
+			response.sendRedirect("homeBO.jsp");
+			break;
+
+		case "logsMenu":
+			response.sendRedirect("index.jsp");
+			break;
+
 		}
+
+	}
+	
+	// Show all client for user logged
+	private void showAllClient(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		allClients.clear();
+		filteredClients.clear();
+		allClients = this.clientServiceDTO.getAllClient();
+		HttpSession session = request.getSession(true);
+		UserDTO userLogged=(UserDTO) session.getAttribute("utente");
 		
-		@Override
-		public void service (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			ServiceDTO<HumanResourceDTO> service = new HumanResourceServiceDTO();
-		    
-		    if(request.getParameter("idHR")!=null) {
-		    	idhr = Integer.parseInt(request.getParameter("idHR").toString());
-		    }
-			
-		    HumanResourceDTO hrdto;
-			boolean ans;
-			int iduser;
-			String name, surname;
-			
-			
-		    String mode = request.getParameter("mode");
-			
-			session = request.getSession();
-			session.setAttribute("idHR",idhr);
-			
-			switch (mode.toUpperCase()) {
-
-			case "HRLISTBY":
-				updateList(request);
-				getServletContext().getRequestDispatcher("/item/hrmanager.jsp").forward(request, response);
-				break;
-
-			case "HRLISTTOP":
-				updateList(request);
-				getServletContext().getRequestDispatcher("/hrhome.jsp").forward(request, response);
-				break;
-
-			case "READ":
-				hrdto = service.read(idhr);
-				request.setAttribute("hrdto", hrdto); //possibile error
-				getServletContext().getRequestDispatcher("/item/hrread.jsp").forward(request, response);
-				break;
-
-			case "INSERT":
-				name= request.getParameter("name");
-				surname= request.getParameter("surname");
-				iduser = Integer.parseInt(request.getParameter("iduser"));
-				hrdto = new HumanResourceDTO (name, surname, iduser);
-				ans = service.insert(hrdto);
-				request.setAttribute("ans", ans);
-				updateList(request);
-				getServletContext().getRequestDispatcher("/item/hrinsert.jsp").forward(request, response);
-				break;
-
-			case "PREUPDATE":
-				hrdto = service.read(idhr);
-				request.setAttribute("hrdto", hrdto);
-				getServletContext().getRequestDispatcher("/item/hrpreupdate.jsp").forward(request, response);
-				break;
-
-			case "UPDATE":
-				name = request.getParameter("name");
-				surname= request.getParameter("surname");
-				iduser = Integer.parseInt(request.getParameter("iduser"));
-				int idToUpdate = Integer.parseInt(request.getParameter("idToUpdate"));
-				hrdto = new HumanResourceDTO(idToUpdate,name, surname, iduser);
-				ans = service.update(hrdto);
-				request.setAttribute("ans", ans);
-				System.out.println(ans);
-				updateList(request);
-				getServletContext().getRequestDispatcher("/item/hrupdate.jsp").forward(request, response);
-				break;
-
-			case "DELETE":
-				hrdto = service.read(idhr);
-				ans = service.delete(hrdto);
-				request.setAttribute("ans", ans);
-				updateList(request);
-				getServletContext().getRequestDispatcher("/item/hrdelete.jsp").forward(request, response);
-				break;
-			}
+		for (ClientDTO clientDTO:allClients) {
+			if (clientDTO.getUserDTO().getId()==userLogged.getId())
+				filteredClients.add(clientDTO);
 		}
+			
+		request.setAttribute("allClient", filteredClients);
+		getServletContext().getRequestDispatcher("/client/manageClient.jsp").forward(request, response);
+	}
 }
 
