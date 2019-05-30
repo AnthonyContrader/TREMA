ba2135d5703f113ebb23fd765f9aae7f624f10f1
Sibillano95 <gianluca.95@hotmail.com>
2019-05-30 15:39:42 +0200

    gianluca

--- src/main/java/it/contrader/servlets/ProjectServlet.java
+++ src/main/java/it/contrader/servlets/ProjectServlet.java
@@ -1,124 +1,117 @@
 package it.contrader.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 import it.contrader.dto.ProjectDTO;
+import it.contrader.dto.UsersDTO;
 import it.contrader.service.ProjectServiceDTO;
-import it.contrader.service.ServiceDTO;
 
-public class ProjectServlet extends HttpServlet{
-	private static final long serialVersionUID = 1L;
-	private HttpSession session;
-	private int iduser;
-	
-	public ProjectServlet() {
-	}
-	
-	public void updateList(HttpServletRequest request) {
-		session.getAttribute("iduser" );
-		ServiceDTO<ProjectDTO> service = new ProjectServiceDTO();
-		List<ProjectDTO> listDTO = service.getAllBy(iduser);
-		request.setAttribute("list", listDTO);
-	}
-	
+public class ProjectServlet extends HttpServlet {
+
+	private ProjectServiceDTO projectServiceDTO = new ProjectServiceDTO();
+	private List<ProjectDTO> allProjects = new ArrayList<ProjectDTO>();
+	private List<ProjectDTO> filteredProjects = new ArrayList<ProjectDTO>();
+
 	@Override
-	public void service (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ServiceDTO<ProjectDTO> service = new ProjectServiceDTO();
-	    
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		if(request.getParameter("iduser")!=null) {
-			iduser = Integer.parseInt(request.getParameter("iduser"));
-	    }
-		
-		ProjectDTO dto;
-		boolean ans;
-		int idproject;
-		String name, description, projectType;
-		
-		
-	    String mode = request.getParameter("mode");
-		
-		session = request.getSession();
-		session.setAttribute("iduser",iduser);
-		
-		switch (mode.toUpperCase()) {
+		final String scelta = request.getParameter("richiesta");
+		final HttpSession session = request.getSession(true);
+		final UsersDTO userLogged = (UsersDTO) session.getAttribute("utente");
 
-		case "PROJECTLISTBY":
-			updateList(request);
-			getServletContext().getRequestDispatcher("/project/projectmanager.jsp").forward(request, response);
+		switch (scelta) {
+
+		case "project_manager":
+			showAllProject(request, response);
 			break;
 
-		case "PROJECTLISTOP":
-			updateList(request);
-			getServletContext().getRequestDispatcher("/homeoperatore.jsp").forward(request, response);
+		case "insertRedirect":
+			response.sendRedirect("project/insertProject.jsp");
 			break;
 
-		case "READ":
-			
-			idproject = Integer.parseInt(request.getParameter("idproject"));
-			
-			ServiceDTO<ProjectDTO> projectservice = new ProjectServiceDTO();
-			List<ProjectDTO>listDTO = projectservice.getAllBy(idproject);
-			request.setAttribute("list", listDTO);
-			
-			session.setAttribute("projectId",idproject);
-			
-			dto = service.read(idproject);
-			request.setAttribute("dto", dto);
-			getServletContext().getRequestDispatcher("/project/readproject.jsp").forward(request, response);
+		case "insert":
+			// final int userId = Integer.parseInt(request.getParameter("user_id"));
+			final String projectName = request.getParameter("name");
+			final String descrizione = request.getParameter("descrizione");
+			final String tipo = request.getParameter("tipo");
+			// UserDTO userInsert = new UserDTO("","","");
+
+			// userInsert.setId(userId);
+			final ProjectDTO projectInsert = new ProjectDTO(projectName,descrizione,tipo,userLogged);
+			projectServiceDTO.insertProject(projectInsert);
+			showAllProject(request, response);
 			break;
 
-		case "INSERT":
-			name= request.getParameter("name");
-			description= request.getParameter("descrizione");
-			projectType= request.getParameter("tipo");
-			int iduser = Integer.parseInt(request.getParameter("iduser"));
-			dto = new ProjectDTO (name, description, projectType, iduser);
-			ans = service.insert(dto);
-			request.setAttribute("ans", ans);
-			updateList(request);
-			getServletContext().getRequestDispatcher("/project/projectmanager.jsp").forward(request, response);
+		case "updateRedirect":
+			int id = Integer.parseInt(request.getParameter("idproject"));
+			ProjectDTO projectUpdate = new ProjectDTO("","","",new UsersDTO("", "", ""));
+			projectUpdate.setId(id);
+
+			projectUpdate = this.projectServiceDTO.readProject(projectUpdate);
+			request.setAttribute("projectUpdate", projectUpdate);
+			getServletContext().getRequestDispatcher("/project/updateProject.jsp").forward(request, response);
+
 			break;
 
-		case "PREUPDATE":
-			idproject = Integer.parseInt(request.getParameter("idproject"));
-			dto = service.read(idproject);
-			dto.setIdproject(idproject);
-			request.setAttribute("dto", dto);
-			getServletContext().getRequestDispatcher("/project/updateproject.jsp").forward(request, response);
+		case "update":
+			final Integer projectIdUpdate = Integer.parseInt(request.getParameter("idproject"));
+			// final Integer userIdUpdate =
+			// Integer.parseInt(request.getParameter("user_id"));
+			final String projectNameUpdate = request.getParameter("name");
+			final String descrizioneUpdate = request.getParameter("descrizione");
+			final String tipoUpdate = request.getParameter("tipo");
+
+			final ProjectDTO projectDTO = new ProjectDTO(projectNameUpdate,descrizioneUpdate,tipoUpdate,userLogged);
+			projectDTO.setId(projectIdUpdate);
+			projectServiceDTO.updateProject(projectDTO);
+			showAllProject(request, response);
 			break;
 
-		case "UPDATE":
-			name = request.getParameter("name");
-			description = request.getParameter("descrizione");
-			projectType= request.getParameter("tipo");
-			iduser = Integer.parseInt(request.getParameter("iduser"));
-			int idToUpdate = Integer.parseInt(request.getParameter("idToUpdate"));
-			dto = new ProjectDTO(idToUpdate,name ,description, projectType, iduser);
-			ans = service.update(dto);
-			request.setAttribute("ans", ans);
-			System.out.println(ans);
-			updateList(request);
-			getServletContext().getRequestDispatcher("/project/projectmanager.jsp").forward(request, response);
+		case "delete":
+			final Integer projectIdDelete = Integer.parseInt(request.getParameter("idproject"));
+
+			final ProjectDTO projectdelete = new ProjectDTO("","","",userLogged);
+			projectdelete.setId(projectIdDelete);
+			projectServiceDTO.deleteProject(projectdelete);
+			showAllProject(request, response);
 			break;
 
-		case "DELETE":
-			idproject = Integer.parseInt(request.getParameter("idproject"));
-			dto = service.read(idproject);
-			ans = service.delete(dto);
-			request.setAttribute("ans", ans);
-			updateList(request);
-			getServletContext().getRequestDispatcher("/project/projectmanager.jsp").forward(request, response);
+		case "indietro":
+			response.sendRedirect("homeAdmin.jsp"); // DA MODIFICARE CON LA VIEW CORRETTA
 			break;
+
+		case "logsMenu":
+			response.sendRedirect("index.jsp");
+			break;
+
 		}
+
 	}
+	
 
-
+	// Show all project for user logged
+	private void showAllProject(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		allProjects.clear();
+		filteredProjects.clear(); 
+		allProjects = this.projectServiceDTO.getAllProject();
+		HttpSession session = request.getSession(true);
+		UsersDTO userLogged=(UsersDTO) session.getAttribute("utente");
+		
+		for (ProjectDTO projectDTO:allProjects) {
+			if (projectDTO.getUserDTO().getId()==userLogged.getId())
+				filteredProjects.add(projectDTO); 
+		}
+			
+		request.setAttribute("allProjects", filteredProjects);
+		getServletContext().getRequestDispatcher("/project/manageProject.jsp").forward(request, response);
+	}
 }
